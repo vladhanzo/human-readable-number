713f20da4fb9e9f7386d5ab03be97c2c95495501
Владислав Капустин <kapustin.vladislav.an@gmail.com>
2021-03-09 10:00:38 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -37,39 +37,39 @@ module.exports = function toReadable(number) {
             "ninety",
         ],
     ];
+
+    return hundreds(number).trim();
+
+    function hundreds(number) {
+        if (number === 0) {
+            return "zero";
+        } else if (number >= 100) {
+            return (
+                num[0][Math.floor(number / 100)] +
+                " hundred" +
+                " " +
+                dozens(Math.floor(number % 100))
+            );
+        } else {
+            return dozens(number);
+        }
+    }
+
+    function dozens(number) {
+        if (number < 10) {
+            return num[0][number];
+        } else if (number <= 99 && number >= 20) {
+            return num[2][Math.floor(number / 10)] + unit(number);
+        } else {
+            return num[1][number - 10];
+        }
+    }
+
+    function unit(number) {
+        if (!number % 10) {
+            return;
+        } else {
+            return " " + num[0][number % 10];
+        }
+    }
 };
-
-return hundreds(number).trim();
-
-function hundreds(number) {
-    if (number === 0) {
-        return "zero";
-    } else if (number >= 100) {
-        return (
-            num[0][Math.floor(number / 100)] +
-            " hundred" +
-            " " +
-            dozens(Math.floor(number % 100))
-        );
-    } else {
-        return dozens(number);
-    }
-}
-
-function dozens(number) {
-    if (number < 10) {
-        return words[0][number];
-    } else if (number <= 99 && number >= 20) {
-        return num[2][Math.floor(number / 10)] + unit(number);
-    } else {
-        return num[1][number - 10];
-    }
-}
-
-function unit(number) {
-    if (!number % 10) {
-        return;
-    } else {
-        return " " + num[0][number % 10];
-    }
-}
